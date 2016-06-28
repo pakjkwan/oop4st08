8418133a9b971f23b12c8fa2c9cb632405a7dcdd
hb <hb@hb2000>
2016-06-28 17:36:57 +0900

    세션 학습

--- oop/src/map/MemberBean.java
+++ oop/src/map/MemberBean.java
@@ -2,4 +2,42 @@ package map;
 
 public class MemberBean {
 	private String id,pw,name,gender;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "회원정보 [ID=" + id + ", 비번=" + pw + ", 이름=" + name + ", 성별=" + gender + "]";
+	}
+	
 }
--- oop/src/map/MemberService.java
+++ oop/src/map/MemberService.java
@@ -1,12 +1,24 @@
 package map;
 
+import java.util.List;
+
 public interface MemberService {
-	public void join();
-	public void login();
-	public void detail();
-	public void list();
-	public void findById();
-	public void findByName();
-	public void updatePW();
-	public void delete();
+	/*
+	"--- 회원이 보는 화면 ---\n"
+	+ "1회원가입 2로그인 3내정보보기(detail) 4내정보수정(비번) 5탈퇴 0종료\n"
+	+ "--- 관리자 화면 ---\n"
+	+ "11회원목록 12검색(ID) 13검색(이름) 14검색(성별) 15회원수 "))
+	 * */
+	public String join(MemberBean member); 
+	public String login(MemberBean member);
+	public MemberBean detail();
+	public void updatePW(MemberBean member);
+	public String delete();
+	public List<MemberBean> list();
+	public MemberBean findById(String id);
+	public List<MemberBean> findByName(String name);
+	public List<MemberBean> findByGender(String gender);
+	public int count();
+	
+	
 }
